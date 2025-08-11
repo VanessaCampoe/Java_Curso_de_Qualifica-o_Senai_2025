b936a15af581f79cb2f13ef45a1fa69087e5a446
VanessaCampoe <vanessacampoe@gmail.com>
2025-08-11 17:41:49 -0300

    POO introdução class, metotodo, atributo  Java 07/08/2025

---
+++ Atividades/03_atividade/03_atividade/src/com/atividade03/app/App.java
@@ -0,0 +1,27 @@
+//TODO - 
+/* crie um programa  com uma classe chamada conta , comcom os seguintes atributos :
+ * Titular da conta 
+ * cpf do titular 
+ * agencia 
+ * Numero da conta saldo 
+ * O usuario devra inofrmar o nome do titular e o cpf , e o progrma a exibe um menu com as seguintes operaçoes :
+ * Exbir dados da conta 
+ * fazer saque 
+ * fazer deposito
+ * sair do programa
+ * 
+ * 
+ */
+
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Olá, não há nessa terra o que eu não possa fazer!");
+
+
+        Conta conta = new Conta();
+        
+    }
+}
+
