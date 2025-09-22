a0d1b2612fe8a4f03029e98484a13faa9cd2524e
VanessaCampoe <vanessacampoe@gmail.com>
2025-09-22 17:42:47 -0300

    morreu  22/09/2025

---
+++ spring/modlarizacao/modlarizacao/src/main/java/com/modlarizacao/app/ModuloController.java
@@ -0,0 +1,16 @@
+package com.modlarizacao.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class ModuloController {
+    @RequestMapping("/")
+    public String index(){
+      return "index";
+    }
+
+
+}
---
+++ spring/modlarizacao/modlarizacao/src/test/java/com/modlarizacao/app/ModlarizacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modlarizacao.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModlarizacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
